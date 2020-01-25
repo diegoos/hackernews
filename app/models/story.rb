cee9899f196139f6c582e5b34163aa39d48777d4
Diego <contato@diegoos.com>
2020-01-25 00:25:11 -0300

    Retrieve stories from lib and not from concern

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,17 +2,15 @@
 
 class Story
   include ActiveModel::Model
-  include HackerNewsApi
-  include RetrieveStories
 
   attr_accessor :by, :descendants, :id, :kids, :score,
                 :time, :title, :type, :url
 
-  def self.get(limit)
+  def self.get(limit = 10)
     @_get ||= begin
-      retrieve_stories(limit).map do |story|
-        Story.new(story)
-      end
-    end
+                HackerNewsApi.retrieve_top_stories(limit).map do |story|
+                  Story.new(story)
+                end
+              end
   end
 end
--- lib/hacker_news_api.rb
+++ lib/hacker_news_api.rb
@@ -5,8 +5,6 @@ module HackerNewsApi
   include HTTParty
   base_uri 'https://hacker-news.firebaseio.com/v0'
 
-  module_function
-
   # Get items from Hacker News API
   def item(id)
     get("/item/#{id}.json")
@@ -14,5 +12,21 @@ module HackerNewsApi
 
   def top_stories
     get('/topstories.json')
+  end
+
+  module_function
+
+  def retrieve_top_stories(limit)
+    stories = top_stories
+
+    return [] unless stories.code == 200
+
+    all_stories = []
+
+    stories.first(limit).each do |story|
+      all_stories << item(story).parsed_response
+    end
+
+    all_stories
   end
 end
